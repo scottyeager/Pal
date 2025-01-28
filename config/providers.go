1e5649ef1a981d66702442e7cbcad04ed111f75d
Scott Yeager <yeagersm@gmail.com>
2025-01-28 14:50:52 -0800

    Update models, disable some providers for now

--- config/providers.go
+++ config/providers.go
@@ -17,30 +17,13 @@ var ProviderTemplates = map[string]Provider{
 		URL: "https://api.deepseek.ai/v1",
 		Models: []string{
 			"deepseek-chat",
-		},
-	},
-	"openai": {
-		URL: "https://api.openai.com/v1",
-		Models: []string{
-			"gpt-3.5-turbo",
-			"gpt-4",
-			"gpt-4-turbo-preview",
-		},
-	},
-	"anthropic": {
-		URL: "https://api.anthropic.com/v1",
-		Models: []string{
-			"claude-2",
-			"claude-instant-1",
-			"claude-3-opus",
-			"claude-3-sonnet",
+			"deepseek-reasoner",
 		},
 	},
 	"huggingface": {
 		URL: "https://api-inference.huggingface.co/v1/",
 		Models: []string{
 			"meta-llama/Llama-3.3-70B-Instruct",
-			"meta-llama/Llama-3.1-8B-Instruct",
 			"meta-llama/Llama-3.2-3B-Instruct",
 			"meta-llama/Llama-2-7b-chat-hf",
 			"deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
