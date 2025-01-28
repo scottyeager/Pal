319d71fc1ea12505d09e8f68306496ed5ef446a5
Scott Yeager <yeagersm@gmail.com>
2025-01-28 14:59:25 -0800

    Also ensure completions dir exists in abbr.fish

--- abbr.fish
+++ abbr.fish
@@ -1,6 +1,7 @@
 function _pal_get_completion
     set -l completions_file ~/.local/share/pal_helper/completions.txt
     set -l line_number (string match -r "$pal_prefix(\d+)" $argv[1] | tail -n1)
+    mkdir -p (dirname $completions_file)
     touch $completions_file
     sed -n {$line_number}p $completions_file
 end
