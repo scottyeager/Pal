e4d2b71c3eec08fd44d9394a1d7c4a5ba7acd84e
Scott Yeager <yeagersm@gmail.com>
2025-02-16 00:04:25 -0800

    feat: add -config flags, fix unused flag vars

--- cmd/root.go
+++ cmd/root.go
@@ -12,23 +12,21 @@ import (
 
 var version string
 
-var fish bool
-var fishAbbr bool
-var zshAbbr bool
-var fishCompletion bool
-var zshCompletion bool
 var temperature float64
 var markdown bool
 
 var userMessage []string
 
 func init() {
-	rootCmd.Flags().BoolVarP(&fish, "version", "V", false, "Print version number")
-	rootCmd.Flags().BoolVar(&fish, "fish", false, "Print fish abbreviation script and completion script, then exit. Output is meant to be sourced by fish")
-	rootCmd.Flags().BoolVar(&zshAbbr, "fish-abbr", false, "Print fish abbreviation script and exit. Output is meant to be sourced by fish")
-	rootCmd.Flags().BoolVar(&zshAbbr, "zsh-abbr", false, "Writes the zsh-abbr plugin to a tmp directory and prints the path, to be sourced by Zsh")
-	rootCmd.Flags().BoolVar(&fishCompletion, "fish-completion", false, "Print fish autocompletion script and exit. Output is meant to be sourced by fish")
-	rootCmd.Flags().BoolVar(&zshCompletion, "zsh-completion", false, "Print zsh autocompletion script and exit. Output is meant to be sourced by zsh")
+	rootCmd.Flags().BoolP("version", "V", false, "Print version number")
+	rootCmd.Flags().Bool("fish", false, "Print fish abbreviation script and completion script, then exit. Output is meant to be sourced by fish")
+	rootCmd.Flags().Bool("zsh", false, "Print zsh abbreviation script and completion script, then exit. Output is meant to be sourced by zsh")
+	rootCmd.Flags().Bool("fish-config", false, "Outputs lines mean to be appended to fish.config, to enable autocompletions and abbreviations")
+	rootCmd.Flags().Bool("zsh-config", false, "Outputs lines mean to be appended to ~/.zshrc, to enable autocompletions and abbreviations")
+	rootCmd.Flags().Bool("fish-abbr", false, "Print fish abbreviation script and exit. Output is meant to be sourced by fish")
+	rootCmd.Flags().Bool("zsh-abbr", false, "Writes the zsh-abbr plugin to a tmp directory and prints the path, to be sourced by Zsh")
+	rootCmd.Flags().Bool("fish-completion", false, "Print fish autocompletion script and exit. Output is meant to be sourced by fish")
+	rootCmd.Flags().Bool("zsh-completion", false, "Print zsh autocompletion script and exit. Output is meant to be sourced by zsh")
 	rootCmd.PersistentFlags().Float64VarP(&temperature, "temperature", "t", 0, "Set the temperature for the AI model, between 0 and 2 (higher values make output more random)")
 	rootCmd.PersistentFlags().BoolVarP(&markdown, "markdown", "m", false, "Toggle markdown formatting in output (inverts your config setting)")
 
@@ -101,6 +99,11 @@ func Execute() {
 			// case the user changed it
 			fmt.Printf("complete -c %s -f", os.Args[0])
 			os.Exit(0)
+		case "--fish-config":
+			fmt.Println("\n# The following line enables autocompletions and abbreviations for pal")
+			fmt.Println("# Just remove or comment the line to undo all changes to your shell")
+			fmt.Printf("%s --fish | source\n", os.Args[0])
+			os.Exit(0)
 		case "--fish-abbr":
 			cfg := config.LoadConfigOrExit()
 			fmt.Println(abbr.GetFishAbbrScript(cfg.AbbreviationPrefix))
@@ -110,6 +113,16 @@ func Execute() {
 			// Disables file name completions. Set command name dynamically in
 			// case the user changed it
 			fmt.Printf("complete -c %s -f", os.Args[0])
+			os.Exit(0)
+		case "--zsh-config":
+			fmt.Println("\n# The following line enables autocompletions and abbreviations for pal")
+			fmt.Println("# Just remove or comment the line to undo all changes to your shell")
+			fmt.Printf("source <(%s --zsh)\n", os.Args[0])
+			os.Exit(0)
+		case "--zsh":
+			cfg := config.LoadConfigOrExit()
+			fmt.Println(abbr.GetZshAbbrScript(cfg.AbbreviationPrefix))
+			rootCmd.GenZshCompletionNoDesc(os.Stdout)
 			os.Exit(0)
 		case "--zsh-abbr":
 			cfg := config.LoadConfigOrExit()
